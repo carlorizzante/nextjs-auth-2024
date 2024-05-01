7e16f890384c907e8a4fdb7313407b638af4b6ea
Carlo Fioretti Rizzante <carlorizzante@gmail.com>
2024-05-01 23:01:43 +0200

    feature(auth): logout button with server action

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,4 +1,7 @@
-import { auth } from '@/auth';
+import {
+  auth,
+  signOut,
+} from '@/auth';
 
 export default async function SettingsPage() {
   const session = await auth();
@@ -8,6 +11,12 @@ export default async function SettingsPage() {
       <h1>Settings</h1>
       <p>Settings page</p>
       <p>{JSON.stringify(session)}</p>
+      <form action={async () => {
+        'use server';
+        await signOut();
+      }}>
+        <button type="submit">Logout</button>
+      </form>
     </div>
   )
 }
